00f48328ac4b73c7ad3aad810cc7cad1f32b7007
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-05-31 14:49:26 -0600

    Added npm install in order to run angular commands.

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -2,6 +2,7 @@ mkdir -p frontendBuild
 rm -rf ./frontendBuild/*
 cd frontend || exit
 rm -rf ./dist
+npm install
 npm run build
 zip -r -j frontend.zip ./dist/frontend/. -x "*.DS_Store"
 mv frontend.zip ../frontendBuild
