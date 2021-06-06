349dbe12d191a7077752ee9a54badb688be07567
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-06-05 19:37:51 -0600

    frontend script update.

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -8,19 +8,4 @@ zip -r -j frontend.zip ./dist/frontend/. -x "*.DS_Store"
 mv frontend.zip ../frontendBuild
 cd ../frontendBuild || exit
 unzip frontend.zip
-for file in ./main.*
-do
-   mv "$file" "main.js"
-done
-for file in ./polyfills.*
-do
-   mv "$file" "polyfills.js"
-done
-for file in ./runtime.*
-do
-   mv "$file" "runtime.js"
-done
-for file in ./styles.*
-do
-   mv "$file" "styles.css"
-done
+rm -f frontend.zip
