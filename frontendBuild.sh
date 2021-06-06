80d72f27930847734adf0f17568fec9b16db782a
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-06-05 19:03:59 -0600

    frontend bash script update.

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -6,5 +6,21 @@ npm install
 npm run build
 zip -r -j frontend.zip ./dist/frontend/. -x "*.DS_Store"
 mv frontend.zip ../frontendBuild
-#cd ../frontendBuild || exit
-#unzip frontend.zip
+cd ../frontendBuild || exit
+unzip frontend.zip
+for file in ./main.*
+do
+   mv "$file" "main.js"
+done
+for file in ./polyfills.*
+do
+   mv "$file" "polyfills.js"
+done
+for file in ./runtime.*
+do
+   mv "$file" "runtime.js"
+done
+for file in ./styles.*
+do
+   mv "$file" "styles.css"
+done
