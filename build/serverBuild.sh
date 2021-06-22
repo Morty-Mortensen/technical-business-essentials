effe004d371edc43ded4e4b3ae174597103d6604
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-06-21 21:05:35 -0600

    backend build script.

--- build/serverBuild.sh
+++ build/serverBuild.sh
@@ -1,6 +1,6 @@
 # Starts from root directory.
 mkdir -p ./build/server
 rm -rf ./build/server/*
-cd ./backend || exit
+cd ./server || exit
 gradle clean shadowJar
 cp ./build/libs/server-all.jar ../build/server
